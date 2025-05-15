827dfc5f7c8016b922aeddd0c9827e9e81b0b2d3
Anton Grachev <gluek.mail@gmail.com>
2025-05-15 14:29:53 +0300

    Решение 3 л.р.

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("Грачев Антон Михайлович")
+	fmt.Println("Вариант 9")
+
+	fmt.Println("y1: ", solve(1.21, 1.1, 0.09))
+	fmt.Println("y2: ", solve(1.76, 1.1, 0.09))
+	fmt.Println("y3: ", solve(2.53, 1.1, 0.09))
+	fmt.Println("y4: ", solve(3.48, 1.1, 0.09))
+	fmt.Println("y5: ", solve(4.52, 1.1, 0.09))
+
+}
+
+func solve(x float64, a float64, b float64) float64 {
+	part1 := math.Log10(math.Pow(x, 2) - 1)
+	part2 := math.Log(a*math.Pow(x, 2)-b) / math.Log(5)
+	y := part1 / part2
+	return y
 }
